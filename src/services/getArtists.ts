e5d867a128209b1286f6c2f0cf5462cad9735a30
genaroibc <bonavitaismael@gmail.com>
2023-08-01 11:02:25 -0300

    [FEAT]: add service to get artists

---
+++ src/services/getArtists.ts
@@ -0,0 +1,35 @@
+type Artist = {
+  id: number
+  name: string
+  cover: string
+  avatar: string
+}
+
+export async function getArtists(): Promise<Artist[]> {
+  return [
+    {
+      id: 1,
+      name: "The Beatles",
+      avatar: "https://doodleipsum.com/200x200/avatar-2",
+      cover: "https://picsum.photos/600/200"
+    },
+    {
+      id: 2,
+      name: "The Rolling Stones",
+      avatar: "https://doodleipsum.com/200x200/avatar-3",
+      cover: "https://picsum.photos/600/200"
+    },
+    {
+      id: 3,
+      name: "The Who",
+      avatar: "https://doodleipsum.com/200x200/avatar-4",
+      cover: "https://picsum.photos/600/200"
+    },
+    {
+      id: 4,
+      name: "The Kinks",
+      avatar: "https://doodleipsum.com/200x200/avatar-5",
+      cover: "https://picsum.photos/600/200"
+    }
+  ]
+}
