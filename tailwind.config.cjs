03b9eb877fa84614c1a15b9b435d7b06cc539518
genaroibc <bonavitaismael@gmail.com>
2023-08-03 20:04:56 -0300

    [FEAT]: add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-0": "var(--gray-0)",
+        "gray-1": "var(--gray-1)",
+        "gray-2": "var(--gray-2)",
+      }
+    }
+  },
+  plugins: []
 }
